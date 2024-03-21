d8289393b364416f2767c5233c4f2f87bcde83fa
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-03-21 19:29:11 +0200

    Dancer

---
+++ Assets/Dancer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dancer : MonoBehaviour
+{
+    public float sensitivity = 1;
+    public float maxSize = 10;
+
+    public void Dance(float volume)
+    {
+	    volume -= 0.15f;
+	    if (volume < 0f) volume = 0f;
+
+	    transform.localScale = Vector3.one * (0.5f + Mathf.Pow(volume,sensitivity) * maxSize);
+
+    }
+}
