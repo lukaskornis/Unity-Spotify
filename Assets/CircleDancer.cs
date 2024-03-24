11cc8044dcb06ea3a21de4b33fa038ac7cb87ddb
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-03-24 15:22:22 +0200

    Circle Dancer Tweaks

--- Assets/CircleDancer.cs
+++ Assets/CircleDancer.cs
@@ -10,6 +10,7 @@ public class CircleDancer : MonoBehaviour
     [Range(-100,100)]public float rotateSpeed = 100;
     [Range(0f,10f)]public float sensitivity = 1;
     [Range(0f,5f)]public float boost = 1;
+    [Range(0, 1)] public float minScale = 0.2f;
 
     void Start()
     {
@@ -21,7 +22,8 @@ public class CircleDancer : MonoBehaviour
             pos.y = Mathf.Sin(angle);
             pos *= radius;
 
-            Instantiate(prefab, pos, Quaternion.identity,transform);
+            var obj = Instantiate(prefab, pos, Quaternion.identity,transform);
+            obj.transform.LookAt(transform);
         }
 
         Analyzer.onVolumeChanged.AddListener(Dance);
@@ -30,6 +32,6 @@ public class CircleDancer : MonoBehaviour
     void Dance(float volume)
     {
         transform.Rotate(0,0,Mathf.Pow(volume * boost,sensitivity) * rotateSpeed);
-        transform.localScale = Vector3.one * volume;
+        transform.localScale = Vector3.one * (volume + minScale);
     }
 }
