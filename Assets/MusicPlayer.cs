4ac6f642802bbc7db17910320b8b0a7dbd664321
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-11-24 12:38:49 +0200

    Detector

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -1,12 +1,27 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MusicPlayer : MonoBehaviour
 {
     AudioSource source;
+    public TMP_Text musicNameText;
+    public TMP_Text musicLengthText;
+    public Transform dancer;
+    public UnityEvent<float> onBeat;
 
     void Start()
     {
         source = GetComponent<AudioSource>();
+        musicNameText.text = source.clip.name;
+        musicLengthText.text = source.clip.length.ToString();
+    }
+
+    void Update()
+    {
+        float[] data = new float[512];
+        source.clip.GetData(data, source.timeSamples);
+        onBeat.Invoke(data[0]);
     }
 
     public void Pause()
